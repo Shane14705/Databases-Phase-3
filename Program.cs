0ae7547847e374451a97e6ac9b4ed6f0ed5bd1cf
Shane McKelvey <psmnhb@umsystem.edu>
2022-12-07 21:03:03 -0600

    Began work on options/pagination helper function.

--- Program.cs
+++ Program.cs
@@ -7,3 +7,14 @@ Console.WriteLine("Hello, World!");
 
 string connstring = File.OpenText("ConnectionString.txt")?.ReadLine();
 Phase3Context Phase3DB = new Phase3Context(connstring);
+
+int OptionsMenu(params string[] options)
+{
+    int i;
+    for (i=0; i < options.Length; i++)
+    {
+        Console.WriteLine(i + ". " + options[0]);
+    }
+
+    return i;
+}
